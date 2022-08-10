24a536a9e1ac1e3c902fa1f4194381d728132b05
Yarmolenko <yarmolenko.dany@gmail.com>
2022-08-10 14:13:46 +0300

    add build folder

--- src/utils/tags.js
+++
@@ -1,3 +0,0 @@
-export const ButtonOpacity = 'button'
-export const Paragraph = 'p'
-
--- src/utils/tags.native.js
+++
@@ -1,4 +0,0 @@
-import { TouchableOpacity, Text } from 'react-native'
-
-export const ButtonOpacity = TouchableOpacity
-export const Paragraph = Text
